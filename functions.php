5f5dc814ec09c27395d371bf88c486f618c549bb
jkajan <jonatan.kajan@gmail.com>
2017-07-17 18:31:23 +0300

    Fix broken header for real this time

--- functions.php
+++ functions.php
@@ -43,18 +43,24 @@ require_once(get_template_directory().'/assets/translation/translation.php');
 require_once(get_template_directory().'/assets/functions/admin.php'); 
 
 function single_archive_title($title) {
-	if (is_category()) {
+    if (is_month()) {
+        $title = get_the_date('F Y'); 
+    } elseif (is_year()) {
+        $title = get_the_date('Y'); 
+    } elseif (is_day()) {
+        $title = get_the_date('F j, Y'); 
+	} elseif (is_tag()) {
+        $title = single_tag_title('#', false);
+	} elseif (is_category()) {
         $title = single_cat_title('', false);
-    } elseif (is_month()) {
-        $title = single_month_title('', false);
     } elseif (is_author()) {
-        $title = '<span class="vcard">'.get_the_author().'</span>';
+        $title = get_the_author();
     } elseif (is_post_type_archive()) {
         $title = post_type_archive_title('', false);
     } elseif (is_tax()) {
         $title = single_term_title('', false);
     } else {
-		$title = get_the_archive_title();
+		$title = "Posts";
 	}
   
     return $title;
